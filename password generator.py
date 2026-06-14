fe2b5fc4f195ed82f9c0aa4d8ee6fe210111b8fe
pushkar662006-star <pushkar662006@gmail.com>
2026-06-14 12:11:59 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(characters) for _ in range(length))
+
+print("Generated password:", password)
